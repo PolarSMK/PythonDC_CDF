703d68609961bae3e954f588d1b86c8ea2c73e92
PolarSMK <mpp47443@gmail.com>
2023-12-06 13:04:32 -0300

    Subido

---
+++ Actividades/Tamagotchi.py
@@ -0,0 +1,19 @@
+class Tamagotchi:
+    def __init__(self,nombre,nivelEnegia,nivelHambre,nivelFelicidad,humor,estaVivo):
+        self.nombre = nombre
+        self.nivelEnergia = nivelEnegia
+        self.nivelHambre = nivelHambre
+        self.nivelFelicidad = nivelFelicidad
+        self.humor = humor
+        self.estaVivo = estaVivo
+    
+    def mostrarEstado(self):
+        pass
+    def alimentar(self):
+        pass
+    def jugar(self):
+        pass
+    def dormir(self):
+        pass
+    def verificarEstado(self):
+        pass
